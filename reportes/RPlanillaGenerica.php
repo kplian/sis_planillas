65f1581f6c31a74ff8115aa76253c6f28b21b6b2
dora <dcastro@boa.bo>
2014-06-17 09:23:11 -0400

    mjorando reporte

--- reportes/RPlanillaGenerica.php
+++ reportes/RPlanillaGenerica.php
@@ -123,6 +123,7 @@ class RPlanillaGenerica extends  ReportePDF {
 		$this->SetMargins(5, 36, 5);
 	}
 	function generarReporte() {
+		$this->etFontSubsetting(false);
 		$this->AddPage();
 		
 		//iniciacion de datos 
