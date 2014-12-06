d3de320b1afb8f27a90b0da4f4d7ebdd63141081
dora <dcastro@boa.bo>
2014-12-06 15:03:22 -0400

    errores en columna_detalle

--- vista/columna_valor/ColumnaValor.php
+++ vista/columna_valor/ColumnaValor.php
@@ -231,7 +231,7 @@ Phx.vista.ColumnaValor=Ext.extend(Phx.gridInterfaz,{
 	east:{
 		  url:'../../../sis_planillas/vista/columna_detalle/ColumnaDetalle.php',
 		  title:'Columna Detalle', 
-		  height:'50%',
+		  weigth:'50%',
 		  cls:'ColumnaDetalle',
 		  collapsed:true
 	},    
