a78b11d1e4fc62734403cb04c136ed3a80355c42
rarteaga <rensi@kplian.com>
2020-02-17 18:27:45 -0400

    #95 no considerar AFP's al insertar empleados en planillas de aguinaldos

--- base/funciones/plani.f_plaguin_insert_empleados.sql
+++ base/funciones/plani.f_plaguin_insert_empleados.sql
@@ -1,8 +1,26 @@
-CREATE OR REPLACE FUNCTION plani.f_plaguin_insert_empleados (
+--------------- SQL ---------------
+
+CREATE OR REPLACE FUNCTION plani.f_plaquin_insert_empleados (
   p_id_planilla integer
 )
 RETURNS varchar AS
 $body$
+ /**************************************************************************
+   PLANI
+  ***************************************************************************
+   SCRIPT:
+   COMENTARIOS:
+   AUTOR: Jaim Rivera (Kplian)
+   DESCRIP:  inserta solo personal vigente que tiene derechi a prima la gestion pasada
+   Fecha: 16-10-2019 
+
+
+    HISTORIAL DE MODIFICACIONES:
+       
+ ISSUE            FECHA:              AUTOR                 DESCRIPCION
+   
+ #95           17-02-2020       Rarteaga KPLIAN     No revisar AFP en registro de funcionarios en planillas de Aguinaldos 
+ ********************************************************************************/
 DECLARE
   v_registros			record;
   v_planilla			record;
@@ -14,116 +32,103 @@ DECLARE
   v_filtro_uo			varchar;
   v_id_afp				integer;
   v_id_cuenta_bancaria	integer;
-  v_fecha_ini			date;
-  v_entra				varchar;
-  v_fecha_fin_planilla	date;
-  v_dias				integer;
-  v_tipo_contrato		varchar;
-  
 BEGIN
 	
-    v_nombre_funcion = 'plani.f_plaguin_insert_empleados';
+    v_nombre_funcion = 'plani.f_plaquin_insert_empleados';
     v_filtro_uo = '';
-	select id_tipo_planilla, per.id_periodo, ges.fecha_ini, ges.fecha_fin, id_uo, 
-    		p.id_usuario_reg,p.fecha_planilla,ges.gestion,p.id_gestion
+	select id_tipo_planilla, per.id_periodo, fecha_ini, fecha_fin, id_uo, p.id_usuario_reg
     into v_planilla 
     from plani.tplanilla p
-    left join param.tperiodo per
+    inner join param.tperiodo per
     	on p.id_periodo = per.id_periodo
-    inner join param.tgestion ges
-    	on ges.id_gestion = p.id_gestion
     where p.id_planilla = p_id_planilla;
-    
-    if (v_planilla.fecha_planilla >= ('20/12/' || v_planilla.gestion)::date) then
-    	v_fecha_fin_planilla = ('31/12/' || v_planilla.gestion)::date;        
-    ELSE
-    	v_fecha_fin_planilla = v_planilla.fecha_planilla;    	
-    end if;
     
     if (v_planilla.id_uo is not null) then
     	v_filtro_uo = ' uofun.id_uo in (' || orga.f_get_uos_x_planilla(v_planilla.id_uo) || ','|| v_planilla.id_uo ||') and ';
     end if;
-        
-        
+    
+    
     for v_registros in execute('
-          select distinct on (uofun.id_funcionario) uofun.id_funcionario , uofun.id_uo_funcionario,ofi.id_lugar,uofun.fecha_asignacion as fecha_ini,
-          (case when (uofun.fecha_finalizacion is null or uofun.fecha_finalizacion > ''' || v_fecha_fin_planilla || ''') then 
-          	''' || v_fecha_fin_planilla || ''' 
-          else 
-          	uofun.fecha_finalizacion 
-          end) as fecha_fin,uofun.fecha_finalizacion as fecha_fin_real 
+          select distinct on (uofun.id_funcionario) uofun.id_funcionario , uofun.id_uo_funcionario,ofi.id_lugar,car.id_cargo,
+          tc.codigo as tipo_contrato,uofun.fecha_finalizacion  
           from orga.tuo_funcionario uofun
           inner join orga.tcargo car
               on car.id_cargo = uofun.id_cargo
           inner join orga.ttipo_contrato tc
               on car.id_tipo_contrato = tc.id_tipo_contrato  
-          inner join orga.toficina ofi
+          left join orga.toficina ofi
               on car.id_oficina = ofi.id_oficina 
           where tc.codigo in (''PLA'', ''EVE'') and UOFUN.tipo = ''oficial'' and ' 
-          	|| v_filtro_uo || ' coalesce(uofun.fecha_finalizacion,''' || v_fecha_fin_planilla || ''') >= (''31/03/' || v_planilla.gestion ||''')::date and 
-            uofun.fecha_asignacion <=  '''|| '31/12/' || v_planilla.gestion || ''' and 
-              uofun.estado_reg != ''inactivo'' 
-              and uofun.id_funcionario not in (
+          	|| v_filtro_uo || ' uofun.fecha_asignacion <= ''' || v_planilla.fecha_fin || ''' and 
+              (uofun.fecha_finalizacion is null or uofun.fecha_finalizacion >= ''' || v_planilla.fecha_ini || ''') AND
+              uofun.estado_reg != ''inactivo'' and uofun.id_funcionario not in (
                   select id_funcionario
                   from plani.tfuncionario_planilla fp
                   inner join plani.tplanilla p
                       on p.id_planilla = fp.id_planilla
                   where 	fp.id_funcionario = uofun.id_funcionario and 
                           p.id_tipo_planilla = ' || v_planilla.id_tipo_planilla || ' and
-                          p.id_gestion = ' || v_planilla.id_gestion || ')
+                          p.id_periodo = ' || v_planilla.id_periodo || ')
           order by uofun.id_funcionario, uofun.fecha_asignacion desc')loop
-    	v_entra = 'si';
-        if (v_fecha_fin_planilla != ('31/12/' || v_planilla.gestion)::date) then
-        	if (v_registros.fecha_fin_real is null or v_registros.fecha_fin_real > v_fecha_fin_planilla)then
-            	v_entra = 'no';
-            end if;
-        end if;
-        v_dias = plani.f_get_dias_aguinaldo(v_registros.id_funcionario, v_registros.fecha_ini, v_registros.fecha_fin);
+    	
+       --#95 se comentara para planillas de aguinaldo no necesitamos el dato de la AFP
+       -- v_id_afp = plani.f_get_afp(v_registros.id_funcionario, v_planilla.fecha_fin);
         
         
-        if (v_dias >= 90  and v_entra = 'si') then
-        	v_id_afp = plani.f_get_afp(v_registros.id_funcionario, v_registros.fecha_fin);
-        	v_id_cuenta_bancaria = plani.f_get_cuenta_bancaria_empleado(v_registros.id_funcionario, v_planilla.fecha_fin);
-             v_tipo_contrato = plani.f_get_tipo_contrato(v_registros.id_uo_funcionario);  
-            INSERT INTO plani.tfuncionario_planilla (
-                id_usuario_reg,					estado_reg,					id_funcionario,
-                id_planilla,					id_uo_funcionario,			id_lugar,
-                forzar_cheque,					finiquito,					id_afp,
-                id_cuenta_bancaria,				tipo_contrato)
-            VALUES (
-                v_planilla.id_usuario_reg,		'activo',					v_registros.id_funcionario,
-                p_id_planilla,					v_registros.id_uo_funcionario,v_registros.id_lugar,
-                'no',							'no',						v_id_afp,
-                v_id_cuenta_bancaria,			v_tipo_contrato)
-            RETURNING id_funcionario_planilla into v_id_funcionario_planilla;
-            
-            for v_columnas in (	select * 
-                                from plani.ttipo_columna 
-                                where id_tipo_planilla = v_planilla.id_tipo_planilla and estado_reg = 'activo' order by orden) loop
-                INSERT INTO 
-                    plani.tcolumna_valor
-                  (
-                    id_usuario_reg,
-                    estado_reg,
-                    id_tipo_columna,
-                    id_funcionario_planilla,
-                    codigo_columna,
-                    formula,
-                    valor,
-                    valor_generado
-                  ) 
-                  VALUES (
-                    v_planilla.id_usuario_reg,
-                    'activo',
-                    v_columnas.id_tipo_columna,
-                    v_id_funcionario_planilla,
-                    v_columnas.codigo,
-                    v_columnas.formula,
-                    0,
-                    0
-                  );
-            end loop;
+        v_id_cuenta_bancaria = plani.f_get_cuenta_bancaria_empleado(v_registros.id_funcionario, v_planilla.fecha_fin);
+        if (v_registros.id_lugar is null) then
+        	raise exception 'El cargo con identificador:% , no tiene una oficina asignada',v_registros.id_cargo;
         end if;
+
+	-- verificar que tendra quincena para este periodo: solo si tiene una asignacion hasta fin de mes
+	-- y tiene un descuento bono para este periodo
+
+	if (exists (	select 1 
+			from plani.tdescuento_bono db
+			inner join plani.ttipo_columna tc on db.id_tipo_columna = tc.id_tipo_columna
+			where db.estado_reg = 'activo' and db.id_funcionario = v_registros.id_funcionario  
+			and tc.codigo = 'QUICENA' and db.fecha_ini <v_planilla.fecha_fin and (db.fecha_fin is null or db.fecha_fin >=v_planilla.fecha_ini)) 
+			and (v_registros.fecha_finalizacion is null or v_registros.fecha_finalizacion >= v_planilla.fecha_fin))then
+         
+		INSERT INTO plani.tfuncionario_planilla (
+			id_usuario_reg,					estado_reg,					id_funcionario,
+		    id_planilla,					id_uo_funcionario,			id_lugar,
+		    forzar_cheque,					finiquito,					id_afp,
+		    id_cuenta_bancaria,				tipo_contrato)
+		VALUES (
+			v_planilla.id_usuario_reg,		'activo',					v_registros.id_funcionario,
+		    p_id_planilla,					v_registros.id_uo_funcionario,v_registros.id_lugar,
+		    'no',							'no',						 NULL,
+		    v_id_cuenta_bancaria,			v_registros.tipo_contrato)
+		RETURNING id_funcionario_planilla into v_id_funcionario_planilla;
+		    
+		for v_columnas in (	select * 
+							from plani.ttipo_columna 
+				    where id_tipo_planilla = v_planilla.id_tipo_planilla and estado_reg = 'activo' order by orden) loop
+			INSERT INTO 
+			plani.tcolumna_valor
+		      (
+			id_usuario_reg,
+			estado_reg,
+			id_tipo_columna,
+			id_funcionario_planilla,
+			codigo_columna,
+			formula,
+			valor,
+			valor_generado
+		      ) 
+		      VALUES (
+			v_planilla.id_usuario_reg,
+			'activo',
+			v_columnas.id_tipo_columna,
+			v_id_funcionario_planilla,
+			v_columnas.codigo,
+			v_columnas.formula,
+			0,
+			0
+		      );
+		end loop;
+	end if;
     end loop;
     return 'exito';
 EXCEPTION
@@ -141,4 +146,5 @@ LANGUAGE 'plpgsql'
 VOLATILE
 CALLED ON NULL INPUT
 SECURITY INVOKER
+PARALLEL UNSAFE
 COST 100;
