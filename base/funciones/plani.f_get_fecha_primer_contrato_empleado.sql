97fd4d3e01c965d9af2ab5920032fd063121c858
mzambrana <mzambrana@endetransmision.bo>
2020-11-20 15:05:19 -0400

    #ETR-1889 modificacion de funcion de obtencion de primer contrato, incluyendo el tipo licencia sin goce de haber

--- base/funciones/plani.f_get_fecha_primer_contrato_empleado.sql
+++ base/funciones/plani.f_get_fecha_primer_contrato_empleado.sql
@@ -1,7 +1,6 @@
 -- FUNCTION: plani.f_get_fecha_primer_contrato_empleado(integer, integer, date)
 
 -- DROP FUNCTION plani.f_get_fecha_primer_contrato_empleado(integer, integer, date);
-
 CREATE OR REPLACE FUNCTION plani.f_get_fecha_primer_contrato_empleado(
 	p_id_uo_funcionario integer,
 	p_id_funcionario integer,
@@ -12,6 +11,12 @@ CREATE OR REPLACE FUNCTION plani.f_get_fecha_primer_contrato_empleado(
     COST 100
     STABLE 
 AS $BODY$
+
+/****************************************************************************
+ HISTORIAL DE MODIFICACIONES:
+ #ISSUE                FECHA                AUTOR               DESCRIPCION
+ #ETR-1889				20.11.2020			MZM-KPLIAN			Adicion de condicion por nueva modalidad de finalziacion de contrato: licencia sin goce de haber
+*****************************************************************************/
 DECLARE
   v_fecha_ini				date;
   v_id_uo_funcionario		integer;
@@ -42,6 +47,22 @@ BEGIN
       inner join orga.ttipo_contrato tc on tc.id_tipo_contrato = car.id_tipo_contrato 
       where uofun.id_funcionario = p_id_funcionario and uofun.fecha_finalizacion = p_fecha_ini - interval '1 day'
       and uofun.estado_reg != 'inactivo' and uofun.tipo = 'oficial' and tc.codigo in ('PLA','EVE');
+      
+      
+      -- 16.11.2020 #ETR-1889
+      if (v_fecha_ini is null) then -- intentar si es licencia sin goce de haber (caso Wilder Ureña)
+      	 	select uofun.id_uo_funcionario, uofun.fecha_asignacion
+			into v_id_uo_funcionario, v_fecha_ini
+      		from orga.tuo_funcionario uofun
+      		inner join orga.tcargo car on car.id_cargo = uofun.id_cargo
+      		inner join orga.ttipo_contrato tc on tc.id_tipo_contrato = car.id_tipo_contrato 
+      		where uofun.id_funcionario = p_id_funcionario and 
+	      	uofun.fecha_finalizacion<p_fecha_ini - interval '1 day' and uofun.observaciones_finalizacion='licencia'  -- licencia sin goce de haber
+	        and uofun.estado_reg != 'inactivo' and uofun.tipo = 'oficial' and tc.codigo in ('PLA','EVE')
+      		order by uofun.fecha_finalizacion desc limit 1;
+      end if;
+      
+      
   end if;
   
   
